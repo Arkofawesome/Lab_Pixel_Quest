f4130990a98bc619682a156941bd5eb8e123552b
Jackson Lee <69437043+Arkofawesome@users.noreply.github.com>
2025-07-10 12:14:52 -0400

    Geo Controller Done

--- Assets/Geo_Quest/Scripts/GeoController.cs
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -5,11 +5,15 @@ using UnityEngine.UIElements;
 
 public class GeoController : MonoBehaviour
 {
+    private Rigidbody2D rb;
     string var1 = "Hello";
-    int value = 3;
+    int x_Value = 0;
+    int y_Value = 0;
+    public int speed = 3;
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         Debug.Log("Hello World!");
         string var2 = "World!";
         Debug.Log(var1 +" " + var2);
@@ -19,8 +23,32 @@ public class GeoController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(value);
-        value++;
-        
+        //if (Input.GetKeyDown(KeyCode.W))
+        //{
+        //    transform.position += new Vector3(0, 1, 0);
+        //}
+        //else if (Input.GetKeyDown(KeyCode.A))
+        //{
+        //    transform.position += new Vector3(-1, 0, 0);
+        //}
+        //else if (Input.GetKeyDown(KeyCode.S))
+        //{
+        //    transform.position += new Vector3(0, -1, 0);
+        //}
+        //else if (Input.GetKeyDown(KeyCode.D))
+        //{
+        //    transform.position += new Vector3(1, 0, 0);
+        //}
+        //rb.velocity = Vector2.left;
+
+        float xInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(xInput * speed, rb.velocity.y);
+        //Debug.Log(xInput);
+
     }// end update
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Hit!");
+    }
 }// end class
