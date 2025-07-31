7bdaadbf05e5a1c205ab442d1868a9844a7441e8
Jackson Lee <69437043+Arkofawesome@users.noreply.github.com>
2025-07-31 09:15:26 -0400

    Not sure

--- Assets/Jackson/Scripts/CarController.cs
+++ Assets/Jackson/Scripts/CarController.cs
@@ -9,16 +9,20 @@ public class PatrolPoints : MonoBehaviour
     public float speed = 5;
     public Transform[] patrolPoints;
     public int patrolIndex = 0;
+    public Renderer render;
     // Start is called before the first frame update
     void Start()
     {
         transform.position = patrolPoints[patrolIndex].position;
         currentTime = waitTime;
+
+        //render = GetComponentInChildren<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
         transform.position = Vector2.MoveTowards(transform.position, patrolPoints[patrolIndex].position,
             speed * Time.deltaTime);
 
@@ -30,6 +34,28 @@ public class PatrolPoints : MonoBehaviour
                 if (patrolIndex >= patrolPoints.Length)
                 {
                     patrolIndex = 0;
+                }
+                if (patrolIndex == 0 )
+                {
+                    //render.enabled = false;
+                    Vector3 direction = patrolPoints[1].position - patrolPoints[0].position;
+                    float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+                    transform.rotation = Quaternion.Euler(0, 0, angle);
+                    patrolIndex = 1;
+                    transform.position = patrolPoints[0].position;
+                }
+                else if (patrolIndex == 2)
+                {
+                    Vector3 direction = patrolPoints[2].position - patrolPoints[1].position;
+                    float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+                    transform.rotation = Quaternion.Euler(0, 0, angle);
+
+                }
+                else if (patrolIndex == 3)
+                {
+                    Vector3 direction = patrolPoints[3].position - patrolPoints[2].position;
+                    float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+                    transform.rotation = Quaternion.Euler(0, 0, angle);
                 }
                 currentTime = waitTime;
             }
